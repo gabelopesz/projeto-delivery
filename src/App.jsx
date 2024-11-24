9a03c1b9f2ce8184a6ddf68ca393cc284a77bbe4
Yasmim-Melo <yasmim.melo280@gmail.com>
2024-11-24 19:12:17 -0300

    Salvar alterações locais antes de pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<Login />} />
+      
+    </Routes>
+  </Router>
   );
 }
 
